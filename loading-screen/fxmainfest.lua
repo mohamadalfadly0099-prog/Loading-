c0c2b23a9f0b97aae160f8091372ad52e084c76d
0xn7r <mohamadalfadly0099@gmail.com>
2026-06-28 04:54:55 -0900

    Add files via upload
    https://discord.gg/Ms5rpnQXh

---
+++ loading-screen/fxmainfest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'صنع ب كل حب 0xn7r'
+version '1.0.0'
+author 'https://discord.gg/Ms5rpnQXh'
+
+loadscreen 'index.html'
+
+file 'index.html'
+
+loadscreen_manual_shutdown 'yes'
+loadscreen_cursor 'yes'
